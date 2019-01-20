6afd7cd5a036c301fa929bdabc080cb43f2c4340
José Miguel <36297047+jmtc7@users.noreply.github.com>
2019-01-20 20:05:17 +0100

    Make the file compilable in Kinetic (OpenCV3)
    Removed the opencv2/highgui dependendence and the instructions that showed the received image as well as the thresholded ones (their only purpose was to help debugging). It compiles, but still needs TEST
    
    NOTE: The package was developed in ROS Indigo, which use OpenCV2 by default, while Kinetic uses OpenCV3. This was the cause that didn't allow to compile the package in Kinetic and the things removed in this commit were the unique incompatibility between the two versions of OCV2 and OCV3.

--- p2_robots_moviles/src/area_publisher.cpp
+++ p2_robots_moviles/src/area_publisher.cpp
@@ -9,7 +9,7 @@
 //Include dependencies
 	#include <ros/ros.h>
 	#include <image_transport/image_transport.h>
-	#include <opencv2/highgui/highgui.hpp>
+//	#include <opencv2/highgui/highgui.hpp>
 	#include <cv_bridge/cv_bridge.h>
 	#include <std_msgs/Float64MultiArray.h>
 
@@ -35,26 +35,26 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 		cv::Mat red1, red2, green, blue;
 		img_bgr = cv_bridge::toCvShare(msg, "bgr8")->image; //Store original image (in BGR)
 		cv::cvtColor(img_bgr, img_hsv, CV_BGR2HSV); //Convert image from BGR to HSV
-		cv::imshow("view", img_bgr); //Show BRG image
+//		cv::imshow("view", img_bgr); //Show BRG image
 
 		//Threshold red colors
 		cv::inRange(img_hsv, cv::Scalar(0, 128, 102), cv::Scalar(20, 255, 255), red1);
 		cv::inRange(img_hsv, cv::Scalar(155, 128, 102), cv::Scalar(180, 255, 255), red2);
 		red1 = red1+red2; //Add the two pictures
-		cv::imshow("red", red1); //Show red colors
+//		cv::imshow("red", red1); //Show red colors
 		areas.data[0] = getArea(red1);
 
 		//Threshold green colors
 		cv::inRange(img_hsv, cv::Scalar(35, 128, 102), cv::Scalar(75, 255, 255), green);
-		cv::imshow("green", green); //Show green colors
+//		cv::imshow("green", green); //Show green colors
 		areas.data[1] = getArea(green);
 
 		//Threshold blue colors
 		cv::inRange(img_hsv, cv::Scalar(80, 128, 102), cv::Scalar(130, 255, 255), blue);
-		cv::imshow("blue", blue); //Show blue colors
+//		cv::imshow("blue", blue); //Show blue colors
 		areas.data[2] = getArea(blue);
 
-		cv::waitKey(30);
+//		cv::waitKey(30);
 	}
 	catch (cv_bridge::Exception& e)
 	{
@@ -80,8 +80,8 @@ int main(int argc, char **argv)
     areas.data.push_back(0.0);
 
   //Initialize OpenCV windows
-    cv::namedWindow("view"); cv::namedWindow("red"); cv::namedWindow("green"); cv::namedWindow("blue");
-    cv::startWindowThread();
+//    cv::namedWindow("view"); cv::namedWindow("red"); cv::namedWindow("green"); cv::namedWindow("blue");
+//    cv::startWindowThread();
 
   //Initialize ROS nodes
     ros::init(argc, argv, "image_listener");
@@ -107,7 +107,7 @@ int main(int argc, char **argv)
     loop_rate.sleep();
   }
 
-  cv::destroyWindow("view"); cv::destroyWindow("red"); cv::destroyWindow("green"); cv::destroyWindow("blue");
+//  cv::destroyWindow("view"); cv::destroyWindow("red"); cv::destroyWindow("green"); cv::destroyWindow("blue");
 }
 
 
